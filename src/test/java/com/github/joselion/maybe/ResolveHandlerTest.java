378dd6277c03521dd54c7144bfce66c5fc823343
JoseLion <joseluis5000l@gmail.com>
2020-05-10 20:18:05 -0500

    Extends resolve handler API

--- src/test/java/com/github/joselion/maybe/ResolveHandlerTest.java
+++ src/test/java/com/github/joselion/maybe/ResolveHandlerTest.java
@@ -2,7 +2,9 @@ package com.github.joselion.maybe;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.InstanceOfAssertFactories.optional;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.EOFException;
 import java.io.IOException;
 
 import com.github.joselion.maybe.helpers.UnitTest;
@@ -66,6 +68,79 @@ public class ResolveHandlerTest {
   }
 
   @Nested
+  class catchError {
+
+    @Nested
+    class when_the_error_is_present {
+
+      @Nested
+      class and_is_instance_of_the_errorType_argument {
+
+        @Test
+        void catches_the_error_and_the_handler_is_applied() {
+          final ResolveHandler<String, IOException> handler = Maybe.resolve(() -> throwingOp())
+            .catchError(IOException.class, error -> {
+              assertThat(error)
+                .isInstanceOf(IOException.class)
+                .hasMessage("FAIL");
+
+              return "OK";
+            });
+
+          assertThat(handler)
+            .extracting(SUCCESS, optional(String.class))
+            .contains("OK");
+  
+          assertThat(handler)
+            .extracting(ERROR, optional(IOException.class))
+            .isEmpty();
+        }
+      }
+
+      @Nested
+      class and_is_NOT_instance_of_the_errorType_argument {
+
+        @Test
+        void the_error_is_NOT_catched_and_the_handler_is_not_applied() {
+          final ResolveHandler<String, IOException> handler = Maybe.resolve(() -> throwingOp())
+            .catchError(EOFException.class, error -> {
+              throw new AssertionError("The handler should not be executed");
+            });
+
+          assertThat(handler)
+            .extracting(SUCCESS, optional(String.class))
+            .isEmpty();
+  
+          assertThat(handler)
+            .extracting(ERROR, optional(IOException.class))
+            .containsInstanceOf(IOException.class)
+            .withFailMessage("FAIL");
+        }
+      }
+    }
+
+    @Nested
+    class when_the_error_is_NOT_present {
+
+      @Test
+      void the_error_handler_is_not_executed() {
+        final ResolveHandler<String, RuntimeException> handler = Maybe.resolve(() -> "OK")
+          .catchError(RuntimeException.class, error -> {
+            throw new AssertionError("The handler should not be executed");
+          });
+
+        assertThat(handler)
+          .extracting(SUCCESS, optional(String.class))
+          .contains("OK");
+
+        assertThat(handler)
+          .extracting(ERROR, optional(IOException.class))
+          .isEmpty();
+      }
+    }
+  }
+
+  @Nested
   class and {
     
     @Nested
@@ -98,7 +173,7 @@ public class ResolveHandlerTest {
   class orDefault {
     
     @Nested
-    class when_the_value_is_presen {
+    class when_the_value_is_present {
 
       @Test
       void returns_the_value() {
@@ -123,6 +198,51 @@ public class ResolveHandlerTest {
       }
     }
   }
+
+  @Nested
+  class orThrow {
+    @Nested
+    class when_the_value_is_present {
+  
+      @Test
+      void returns_the_value() throws EOFException {
+        assertThat(
+          Maybe.resolve(() -> "OK").orThrow()
+        )
+        .isEqualTo("OK");
+
+        assertThat(
+          Maybe.resolve(() -> "OK").orThrow(error -> new EOFException(error.getMessage()))
+        )
+        .isEqualTo("OK");
+      }
+    }
+
+    @Nested
+    class when_the_value_is_NOT_present {
+
+      @Test
+      void throws_the_error() {
+        final ResolveHandler<?, IOException> handler = Maybe.resolve(() -> throwingOp());
+
+        assertThat(
+          assertThrows(IOException.class, handler::orThrow)
+        )
+        .isExactlyInstanceOf(IOException.class)
+        .hasMessage("FAIL");
+
+        assertThat(
+          assertThrows(
+            IOException.class,
+            () -> handler.orThrow(error -> new EOFException(error.getMessage() + " - OTHER ERROR"))
+          )
+        )
+        .isExactlyInstanceOf(EOFException.class)
+        .hasMessage("FAIL - OTHER ERROR");
+      }
+    }
+  }
+
 
   private String throwingOp() throws IOException {
     throw new IOException("FAIL");
